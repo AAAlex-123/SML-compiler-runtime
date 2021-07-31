ee7c354676b5945a907f76dc95c8c870d4353607
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-07-31 11:32:41 +0300

    works with requirements
    see compiler and executor for details

--- src/sml_package/SML_Simulator.java
+++ src/sml_package/SML_Simulator.java
@@ -1,71 +1,66 @@
 package sml_package;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import requirement.requirements.Requirements;
+
 public class SML_Simulator {
-	static final Scanner scanner = new Scanner(System.in);
 
-	static final String SimpleFile = "C:\\Users\\Danae\\Desktop\\projects\\Java\\SML_new\\Simple.txt";
-	static final String SMLFile = "C:\\Users\\Danae\\Desktop\\projects\\Java\\SML_new\\SML.txt";
-	static final String OutputFile = "C:\\Users\\Danae\\Desktop\\projects\\Java\\SML_new\\out.txt";
-//	static final String SimpleFile = "C:\\Users\\JIM\\Desktop\\java stuff\\SML\\Simple.txt";
-//	static final String SMLFile = "C:\\Users\\JIM\\Desktop\\java stuff\\SML\\SML.txt";
-//	static final String OutputFile = "C:\\Users\\JIM\\Desktop\\java stuff\\SML\\out.txt";
+	private static final Scanner scanner = new Scanner(System.in);
 
-	static final String msg = 
-			"Usage:\n" +
-			"       help    [command]\n" +
-			"   or  compile [options]\n" +
-			"   or  execute [options]\n" +
-			"   or  com_exe [options]\n" +
-			"   or  exit\n" +
-            "\n" +
-			"where options include:\n" +
-			"   -screen\n" +
-			"       write SML and/or program output to screen, independent from file\n" +
-			"\n" +
-			"   -manual\n" +
-			"       write Simple code or SML code by hand, overwrites file\n" +
-            "\n" +
-			"   --input <filename>\n" +
-			"       where the data for compilation or execution are stored\n" +
-			"\n" +
-			"   --output <filename>\n" +
-			"       where the results after compilation or execution are stored\n" +
-			"\n" +
-			"   --inter <filename>\n" +
-			"       only used at com_exe, the file where SML stored after compilation and read for execution\n" +
-            "\n" +
-			"where command is one of:\n" +
-			"   compile, execute, com_exe, exit, help\n" + 
-			"compile --input src.txt -screen\n\n";
+	private static final String msg = "Usage:\n" +
+	        "       help    [command]\n" +
+	        "   or  compile [options]\n" +
+	        "   or  execute [options]\n" +
+	        "   or  com_exe [options]\n" +
+	        "   or  exit\n" +
+	        "\n" +
+	        "where options include:\n" +
+	        "   -screen\n" +
+	        "       write SML and/or program output to screen, independent from file\n" +
+	        "\n" +
+	        "   -st\n" +
+	        "       only used on when compiling, show the SymbolTable at the end.\n" +
+	        "\n" +
+	        "   -manual\n" +
+	        "       write Simple code or SML code by hand, overwrites file\n" +
+	        "\n" +
+	        "   --input <filename>\n" +
+	        "       where the data for compilation or execution are stored\n" +
+	        "\n" +
+	        "   --output <filename>\n" +
+	        "       where the results after compilation or execution are stored\n" +
+	        "\n" +
+	        "   --inter <filename>\n" +
+	        "       only used at com_exe, the file where SML stored after compilation and read for execution\n"
+	        +
+	        "\n" +
+	        "where command is one of:\n" +
+	        "   compile, execute, com_exe, exit, help\n";
 
-	static String user_input, command="", helpCommand, sourceFile, machineFile, outputFile;
-	static String[] tokens;
-	
-	static void zzz() {while(true) {;}}
-	
-	static void dummy(Class<?> c) {
-		System.out.println(c);
-	}
-	
-	public static void main(String[] args) throws IOException {
-		
-		
-		
+	public static void main(String[] args) {
+
 		System.out.println(msg);
-//		System.out.println(System.getProperty("user.dir"));
-		
+
+		String   userInput;
+		String[] tokens;
+		String   command = "";
+
+		Requirements compileReqs = SML_Compiler.getRequirements();
+		Requirements executeReqs = SML_Executor.getRequirements();
+
 		while (!command.equals("exit")) {
+			compileReqs.clear();
+			executeReqs.clear();
+
 			System.out.print("=> ");
-//			"com_exe --input Simple.txt --inter SML.txt --output out.txt -screen";
-			user_input = scanner.nextLine();
-			tokens = user_input.split(" ");
-			
+
+			userInput = scanner.nextLine().trim();
+			tokens = userInput.split(" ");
 			command = tokens[0];
-			HashMap<String, String> options = new HashMap<String, String>(5, 1.0f);
+
+			HashMap<String, String> options = new HashMap<>(7, 1.0f);
 			options.put("-screen", "false");
 			options.put("-manual", "false");
 			options.put("-st", "false");
@@ -73,92 +68,86 @@ public class SML_Simulator {
 			options.put("--input", "");
 			options.put("--output", "");
 			options.put("--inter", "");
-			
+
 			if (command.equals("help")) {
-				try {
-					helpCommand = tokens[1];
-					if (helpCommand.equals("compile"))
-						System.out.println("Use this command to compile");
-					
-					else if (helpCommand.equals("execute"))
-						System.out.println("Use this command to execute");
-					
-					else if (helpCommand.equals("com_exe"))
-						System.out.println("Use this command to compile and execute");
-
-					else if (helpCommand.equals("exit"))
-						System.out.println("Use this command to exit");
-					
-					else if (helpCommand.equals("help"))
-						System.out.println("Use this command to get some help");
-					
-					else if (helpCommand.equals("owo"))
-						System.out.println("uwu");
-					
-					else System.out.println("Unknown command");
-
-				} catch (IndexOutOfBoundsException ioob) {
+				if (tokens.length == 1)
 					System.out.println("Use help [command] to get help on a specific command");
-				}
+				else
+					printHelpForCommand(tokens[1]);
 
 			} else {
-				int i=1;
-				while (i < tokens.length) {
-					if (tokens[i].startsWith("--")) options.put(tokens[i], tokens[++i]);
-					else if (tokens[i].startsWith("-")) options.put(tokens[i], "true");
-					else System.out.println("Error: invalid option: " + tokens[i]);
-					i++;
+
+				// parse args
+				for (int i = 1; i < tokens.length; ++i) {
+					if (tokens[i].startsWith("--"))
+						options.put(tokens[i], tokens[++i]);
+					else if (tokens[i].startsWith("-"))
+						options.put(tokens[i], "true");
+					else
+						System.out.println("Error: invalid option: " + tokens[i]);
 				}
-				
-				if (command.equals("compile")) {											// COMPILE
-					int result = SML_Compiler.compile(options);
-					if (result == 1)
-						System.out.println("\nDue to above errors compilation failed :(\n");
-					else 
-						System.out.println("\nCompilation successful :)\n");
-					
-				} else if (command.equals("execute")) {										// EXECUTE
-					int result = SML_Executor.execute(options);
-					if (result == 1)
-						System.out.println("\nDue to above errors execution failed :(\n");
-					else 
-						System.out.println("\nExecution successful :)\n");
-					
-				} else if (command.equals("com_exe")) {										// COM_EXE
+
+				// do stuff according to command
+				if (command.equals("compile")) { // COMPILE
+
+					compileReqs.fulfil("input", options.get("--input"));
+					compileReqs.fulfil("output", options.get("--output"));
+					compileReqs.fulfil("screen", options.get("-screen").equals("true"));
+					compileReqs.fulfil("st", options.get("-st").equals("true"));
+
+					SML_Compiler.compile(compileReqs);
+
+				} else if (command.equals("execute")) {
+
+					executeReqs.fulfil("input", options.get("--input"));
+					executeReqs.fulfil("output", options.get("--output"));
+					executeReqs.fulfil("screen", options.get("-screen").equals("true"));
+
+					SML_Executor.execute(executeReqs);
+
+				} else if (command.equals("com_exe")) { // COM_EXE
+
+					boolean screen = options.get("-screen").equals("true");
+
 					// compile
-					String out = options.get("--output");
-					options.put("--output", options.get("--inter"));
-	
-					int result1 = SML_Compiler.compile(options);
-					if (result1 == 1)
-						System.out.println("\nDue to above errors compilation failed :(\n");
-					else 
-						System.out.println("\nCompilation successful :)\n");
-	
+					compileReqs.fulfil("input", options.get("--input"));
+					compileReqs.fulfil("output", options.get("--inter"));
+					compileReqs.fulfil("screen", screen);
+					SML_Compiler.compile(compileReqs);
+
 					// execute
-					options.put("-manual", "false");
-					options.put("--input", options.get("--output"));
-					options.put("--output", out);
-					int result2 = SML_Executor.execute(options);
-					if (result2 == 1)
-						System.out.println("\nDue to above errors execution failed :(\n");
-					else 
-						System.out.println("\nExecution successful :)\n");
-					
-				} else if (command.equals("exit")) {										// EXIT
-					;
-					
-				} else if (command.equals("dump")) {										// DUMP
-					System.out.println(options);
-					System.out.println(SML_Compiler.symbolTable);
-					SML_Executor.writeToScreen();
-				} else if (command.equals("")) {
+					executeReqs.fulfil("input", options.get("--inter"));
+					executeReqs.fulfil("output", options.get("--output"));
+					executeReqs.fulfil("screen", screen);
+					SML_Executor.execute(executeReqs);
+
+				} else if (command.equals("exit") || command.equals("")) {
 					;
 				} else {
-					System.out.println("Simulator Error: Unknown command");					// UNKNOWN
+					System.out.println("Simulator Error: Unknown command");
 				}
 			}
 		}
 		System.out.println("good bye :(");
+	}
+
+	private static void printHelpForCommand(String command) {
+		if (command.equals("compile"))
+			System.out.println("Use this command to compile");
+
+		else if (command.equals("execute"))
+			System.out.println("Use this command to execute");
+
+		else if (command.equals("com_exe"))
+			System.out.println("Use this command to compile and execute");
+
+		else if (command.equals("exit"))
+			System.out.println("Use this command to exit");
+
+		else if (command.equals("help"))
+			System.out.println("Use this command to get some help");
+
+		else
+			System.out.println("Unknown command: " + command);
 	}
 }
