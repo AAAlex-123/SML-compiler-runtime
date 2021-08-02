b5893ec42218bef11629cd57f1afd75e7ec66184
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-02 09:06:09 +0300

    Default Memory (#16)
     - added default methods to interfaces
     - TODO: remove them from compiler/executor in another branch

--- src/memory/CodeWriter.java
+++ src/memory/CodeWriter.java
@@ -1,15 +1,43 @@
 package memory;
 
 public interface CodeWriter extends RAM {
-	public int writeInstruction(int value);
+	int writeInstruction(int value);
 
-	public int writeConstant(int value);
+	int writeConstant(int value);
 
-	public int assignPlaceForVariable();
+	int assignPlaceForVariable();
 
-	public void initializeForWriting();
+	void initializeForWriting();
 
-	public int getInstructionCounter();
+	int getInstructionCounter();
 
-	public String list();
+	default String list() {
+		final StringBuilder sb = new StringBuilder(size() * 6);
+		for (int i = 0; i < size(); ++i)
+			sb.append(String.format("%04x%n", read(i)));
+
+		return sb.toString();
+	}
+
+	default String listShort() {
+		final StringBuilder sb = new StringBuilder();
+
+		boolean zeros = false;
+		for (int i = 0, size = size(); i < size; i++) {
+
+			final int val = read(i);
+
+			// replace any number of consecutive zeros with '** ****'
+			if ((val == 0) && !zeros) {
+				zeros = true;
+				sb.append(RAM.sf("** ****"));
+			} else if (val != 0)
+				zeros = false;
+
+			if (!zeros)
+				sb.append(RAM.sf("%02x %04x\n", i, val));
+		}
+
+		return sb.toString();
+	}
 }
