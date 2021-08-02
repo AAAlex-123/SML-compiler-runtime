03e7fc3b2b80d7332e96e2781b3ac3c98876e963
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-02 09:03:07 +0300

    added default method dump

--- src/memory/CodeReader.java
+++ src/memory/CodeReader.java
@@ -1,11 +1,34 @@
 package memory;
 
 public interface CodeReader extends RAM {
-	public int fetchInstruction();
+	int fetchInstruction();
 
-	public void initialiseForExecution();
+	void initialiseForExecution();
 
-	public int getInstructionPointer();
+	int getInstructionPointer();
 
-	public void setInstructionPointer(int address);
+	void setInstructionPointer(int address);
+
+	default String dump() {
+		final StringBuilder sb = new StringBuilder();
+
+		// TODO: fix hard-coded size 16
+		for (int i = 0; i < 16; i++)
+			sb.append(RAM.sf("     %x", i));
+		sb.append("\n");
+
+		for (int i = 0; i < 16; i++) {
+			sb.append(RAM.sf("%x0", i));
+			for (int j = 0; j < 16; j++) {
+				final int val = read((16 * i) + j);
+				sb.append(" ")
+				        .append(val < 0 ? "-" : "+")
+				        .append(RAM.sf("%04x", val));
+			}
+
+			sb.append("\n");
+		}
+
+		return sb.toString();
+	}
 }
