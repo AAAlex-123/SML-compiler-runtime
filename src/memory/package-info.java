b9bb7b459ca2bbae3104ffd4872fb9aad96006d3
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-07 16:05:06 +0300

    added doc

--- src/memory/package-info.java
+++ src/memory/package-info.java
@@ -1,1 +1,10 @@
+/**
+ * A collection of interfaces that define the behaviour of a {@link memory.RAM
+ * Random Access Memory} object and provide additional convenience methods for
+ * {@link memory.CodeReader reading} code from RAM and {@link memory.CodeWriter
+ * writing} code to RAM. Additionally, this package contains an
+ * {@link memory.Memory implementation} of these interfaces.
+ *
+ * @author Alex Mandelias
+ */
 package memory;
