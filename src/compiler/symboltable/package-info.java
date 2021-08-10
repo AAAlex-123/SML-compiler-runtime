47c5aacf1cdfb688eafb75db0b9285a9576136ab
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-10 12:18:56 +0300

    Create package-info.java

---
+++ src/compiler/symboltable/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Defines a {@link compiler.symboltable.SymbolTable SymbolTable} which is used
+ * during compilation to keep track of {@link compiler.symboltable.SymbolInfo
+ * information} about different Symbols found in a high-level-language program.
+ *
+ * @author Alex Mandelias
+ */
+package compiler.symboltable;
