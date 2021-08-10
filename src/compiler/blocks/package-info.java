de425fcde1fdb16324904408416a9e61f97d7c39
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-10 12:56:49 +0300

    Create package-info.java

---
+++ src/compiler/blocks/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Defines a collection of {@link compiler.blocks.Block Blocks}, that are used
+ * to store the locations of instructions. These locations are later retrieved
+ * to fill incomplete branch instructions.
+ *
+ * @author Alex Mandelias
+ */
+package compiler.blocks;
