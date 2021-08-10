3628433e4e59d53ae396cbc8cf479b498f1b4923
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-10 12:16:14 +0300

    Create SymbolKey.java

---
+++ src/compiler/symboltable/SymbolKey.java
@@ -0,0 +1,50 @@
+package compiler.symboltable;
+
+/**
+ * A key for high-level-language Symbols. If the {@link SymbolInfo information}
+ * of two Symbols have the same Key, they are considered identical.
+ *
+ * @author Alex Mandelias
+ */
+class SymbolKey {
+
+	/** The Symbol */
+	public final String     symbol;
+
+	/** The Type of the Symbol */
+	public final SymbolType type;
+
+	/**
+	 * Creates a SymbolKey with the {@code information} about a Symbol.
+	 *
+	 * @param info the information
+	 */
+	public SymbolKey(SymbolInfo info) {
+		this(info.symbol, info.type);
+	}
+
+	/**
+	 * Creates a SymbolKey with a specific {@code symbol} and {@code type}.
+	 *
+	 * @param symbol the symbol
+	 * @param type   the type
+	 */
+	public SymbolKey(String symbol, SymbolType type) {
+		this.symbol = symbol;
+		this.type = type;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (!(other instanceof SymbolKey))
+			return false;
+
+		SymbolKey other1 = (SymbolKey) other;
+		return (other1.symbol.equals(symbol)) && (other1.type == type);
+	}
+
+	@Override
+	public int hashCode() {
+		return symbol.hashCode() + type.hashCode();
+	}
+}
