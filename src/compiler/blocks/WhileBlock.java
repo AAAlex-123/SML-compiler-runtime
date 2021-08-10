f8e4e73d92df007643a384be9a9823dc4490c77a
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-10 12:56:21 +0300

    moved to correct package and doc

---
+++ src/compiler/blocks/WhileBlock.java
@@ -0,0 +1,15 @@
+package compiler.blocks;
+
+/**
+ * Block for code in a {@code while} statement.
+ *
+ * @author Alex Mandelias
+ */
+public class WhileBlock extends Block {
+
+	/**
+	 * The location of the first instruction of the while loop in memory, the
+	 * instruction that starts the calculation of the condition.
+	 */
+	public int locationOfFirstInstruction;
+}
