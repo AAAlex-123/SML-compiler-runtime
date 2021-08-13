eb656183ec709c25dc9e2e975e688ffdf2ea2560
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-13 12:39:21 +0300

    moved to correct package + doc

---
+++ src/compiler/exceptions/VariableNotDeclaredException.java
@@ -0,0 +1,18 @@
+package compiler.exceptions;
+
+/**
+ * Thrown when a Variable that has not been declared is referenced.
+ *
+ * @author Alex Mandelias
+ */
+public class VariableNotDeclaredException extends CompilerException {
+
+	/**
+	 * Constructs the exception with the {@code variable} that has not been declared
+	 *
+	 * @param variable the variable that has not been declared
+	 */
+	public VariableNotDeclaredException(String variable) {
+		super("variable '%s' is not declared", variable);
+	}
+}
