3db51bf79b1ef391016a0371c38338a8ebadcfb0
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-13 12:21:42 +0300

    Create CompilerException.java

---
+++ src/compiler/exceptions/CompilerException.java
@@ -0,0 +1,21 @@
+package compiler.exceptions;
+
+/**
+ * A superclass for all of the Exceptions that may occur during compilation of a
+ * program written in the high-level language.
+ *
+ * @author Alex Mandelias
+ */
+public abstract class CompilerException extends Exception {
+
+	/**
+	 * Constructs the {@code CompilerException} by formatting the {@code text} with
+	 * the {@code args} are provided by the constructors of the concrete subclasses.
+	 *
+	 * @param text a short and descriptive message
+	 * @param args the format arguments
+	 */
+	public CompilerException(String text, Object... args) {
+		super(String.format(text, args));
+	}
+}
