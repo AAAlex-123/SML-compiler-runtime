44291ea2dff01c9499b1181b0c7d871dd35c6ae6
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-18 13:58:18 +0300

    changed line to label

--- src/compiler/exceptions/NotALineException.java
+++ src/compiler/exceptions/NotALineException.java
@@ -1,19 +1,19 @@
 package compiler.exceptions;
 
 /**
- * Thrown when a symbol that is expected to be a Line, is not a Line.
+ * Thrown when a symbol that is expected to be a Label, is not a Label.
  *
  * @author Alex Mandelias
  */
-public class NotALineException extends CompilerException {
+public class NotALabel extends CompilerException {
 
 	/**
 	 * Constructs the exception with information about the {@code symbol} that isn't
-	 * a Line.
+	 * a Label.
 	 *
-	 * @param symbol the symbol that isn't a line
+	 * @param symbol the symbol that isn't a label
 	 */
-	public NotALineException(String symbol) {
-		super("'%s' is not a line", symbol);
+	public NotALabel(String symbol) {
+		super("'%s' is not a label", symbol);
 	}
 }
