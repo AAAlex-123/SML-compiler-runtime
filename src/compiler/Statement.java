6ffbed3c00f88f4ed4d097f57cf50f11f53a2ccb
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-22 14:30:45 +0300

    added syntax checks for each statement

--- src/compiler/Statement.java
+++ src/compiler/Statement.java
@@ -1,5 +1,6 @@
 package compiler;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,11 +8,16 @@ import java.util.Map;
 import compiler.blocks.Block;
 import compiler.blocks.IfBlock;
 import compiler.blocks.WhileBlock;
+import compiler.exceptions.CompilerException;
+import compiler.exceptions.InvalidConditionException;
+import compiler.exceptions.InvalidStatementException;
+import compiler.exceptions.UnexpectedTokenException;
 import compiler.postfix.InfixToPostfix;
 import compiler.postfix.PostfixEvaluator;
 import compiler.postfix.Token;
 import compiler.symboltable.SymbolInfo;
 import compiler.symboltable.SymbolTable;
+import compiler.symboltable.SymbolType;
 import runtime.Instruction;
 
 /**
@@ -30,15 +36,29 @@ enum Statement {
 		public void evaluate(String line, SML_Compiler compiler) {
 
 		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+
+		}
 	},
 
 	/** Declaration of integer variables */
 	INT("int", -1, true) {
 		@Override
 		public void evaluate(String line, SML_Compiler compiler) {
-			final String[] tokens = line.split(" ");
-			for (int i = 2, count = tokens.length; i < count; ++i)
-				compiler.declareVariable(tokens[i], identifier);
+			for (String var : vars(line))
+				compiler.declareVariable(var, identifier);
+		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+			for (String var : vars(line))
+				SymbolType.assertType(var, SymbolType.VARIABLE);
+		}
+
+		private String[] vars(String line) {
+			return Arrays.asList(line.split(" ")).subList(2, line.split(" ").length).toArray(String[]::new);
 		}
 	},
 
@@ -46,11 +66,20 @@ enum Statement {
 	INPUT("input", -1, false) {
 		@Override
 		public void evaluate(String line, SML_Compiler compiler) {
-			final String[] vars = line.substring(9).split(" ");
-			for (final String var : vars) {
+			for (final String var : vars(line)) {
 				final int location = compiler.getVariable(var).location;
 				compiler.addInstruction(Instruction.READ_INT.opcode() + location);
 			}
+		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+			for (String var : vars(line))
+				SymbolType.assertType(var, SymbolType.VARIABLE);
+		}
+
+		public String[] vars(String line) {
+			return Arrays.asList(line.split(" ")).subList(2, line.split(" ").length).toArray(String[]::new);
 		}
 	},
 
@@ -59,17 +88,8 @@ enum Statement {
 		@Override
 		public void evaluate(String line, SML_Compiler compiler) {
 			// generate postfix
-			final String        infix        = line.split("=")[1];
-			final List<Token>   postfix      = InfixToPostfix.convertToPostfix(infix);
-
-			// declare constants that weren't interpreted as constants (e.g. 2+3)
-			for (Token token : postfix) {
-				final String symbol = token.value;
-				if (symbol.matches("\\d+"))
-					if (!compiler.constantDeclared(symbol))
-						compiler.declareConstant(symbol, INT.identifier);
-			}
-
+			final String      infix   = infix(line);
+			final List<Token> postfix = InfixToPostfix.convertToPostfix(infix);
 			final SymbolTable symbolTable = compiler.getSymbolTable();
 			final List<Integer> instructions = PostfixEvaluator.evaluatePostfix(postfix,
 					symbolTable, compiler);
@@ -79,10 +99,36 @@ enum Statement {
 				compiler.addInstruction(instruction);
 
 			// store result of postfix that is loaded after the last instruction of evaluation
-			final String var      = line.split(" ")[2];
+			final String var   = var(line);
 			final int location = compiler.getVariable(var).location;
 
 			compiler.addInstruction(Instruction.STORE.opcode() + location);
+		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+			String[] tokens = line.split(" ");
+
+			SymbolType.assertType(tokens[2], SymbolType.VARIABLE);
+
+			if (!tokens[3].equals("="))
+				throw new UnexpectedTokenException(tokens[3], "'='");
+
+			final List<Token> postfix = InfixToPostfix.convertToPostfix(infix(line));
+
+			for (Token token : postfix) {
+				if (!token.isOperator() || (token == Token.LEFT_PAREN) || (token == Token.RIGHT_PAREN)) {
+					SymbolType.assertNot(token.value, SymbolType.LABEL);
+				}
+			}
+		}
+
+		private String var(String line) {
+			return line.split(" ")[2];
+		}
+
+		private String infix(String line) {
+			return line.split("=")[1];
 		}
 	},
 
@@ -90,8 +136,7 @@ enum Statement {
 	PRINT("print", -1, false) {
 		@Override
 		public void evaluate(String line, SML_Compiler compiler) {
-			final String[] vars = line.substring(9).split(" ");
-			for (final String var : vars) {
+			for (final String var : vars(line)) {
 				final SymbolInfo info = compiler.getSymbol(var);
 
 				final String varType  = info.varType;
@@ -101,15 +146,32 @@ enum Statement {
 					compiler.addInstruction(Instruction.WRITE_NL.opcode() + location);
 			}
 		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+			for (String var : vars(line))
+				SymbolType.assertNot(var, SymbolType.LABEL);
+		}
+
+		private String[] vars(String line) {
+			return Arrays.asList(line.split(" ")).subList(2, line.split(" ").length).toArray(String[]::new);
+		}
 	},
 
 	/** Define a label (location to jump to) */
 	LABEL("label", 3, true) {
 		@Override
 		public void evaluate(String line, SML_Compiler compiler) {
-			final String[] tokens = line.split(" ");
-			final String label = tokens[2];
-			compiler.declareLabel(label);
+			compiler.declareLabel(labelToJump(line));
+		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+			SymbolType.assertType(labelToJump(line), SymbolType.LABEL);
+		}
+
+		private String labelToJump(String line) {
+			return line.split(" ")[2];
 		}
 	},
 
@@ -117,12 +179,19 @@ enum Statement {
 	GOTO("goto", 3, false) {
 		@Override
 		public void evaluate(String line, SML_Compiler compiler) {
-			final String[] tokens = line.split(" ");
-
 			final int location = compiler.addInstruction(Instruction.BRANCH.opcode());
-			final String labelToJump = tokens[2];
+			final String labelToJump = labelToJump(line);
 
 			compiler.setLabelToJump(location, labelToJump);
+		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+			SymbolType.assertType(labelToJump(line), SymbolType.LABEL);
+		}
+
+		private String labelToJump(String line) {
+			return line.split(" ")[2];
 		}
 	},
 
@@ -130,22 +199,23 @@ enum Statement {
 	IFGOTO("ifg", 7, false) {
 		@Override
 		public void evaluate(String line, SML_Compiler compiler) {
-			final String[] tokens = line.split(" ");
 
-			String    op1, op2;
 			int       loc1, loc2;
 			Condition condition;
-			String    labelToJump;
+			String labelToJump;
 
-			op1 = tokens[2];
-			loc1 = compiler.getSymbol(op1).location;
+			loc1 = compiler.getSymbol(op1(line)).location;
 
-			condition = Condition.of(tokens[3]);
+			try {
+				condition = Condition.of(condition(line));
+			} catch (InvalidConditionException e) {
+				// checkSyntax was called, this should never throw
+				throw new RuntimeException(e);
+			}
 
-			op2 = tokens[4];
-			loc2 = compiler.getSymbol(op2).location;
+			loc2 = compiler.getSymbol(op2(line)).location;
 
-			labelToJump = tokens[6];
+			labelToJump = labelToJump(line);
 
 			int location;
 			switch (condition) {
@@ -195,6 +265,34 @@ enum Statement {
 				break;
 			}
 		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+			SymbolType.assertNot(op1(line), SymbolType.LABEL);
+			SymbolType.assertNot(op2(line), SymbolType.LABEL);
+			SymbolType.assertType(labelToJump(line), SymbolType.LABEL);
+			Condition.of(condition(line));
+
+			String goto_ = line.split(" ")[5];
+			if (!goto_.split(" ")[5].equals("goto"))
+				throw new UnexpectedTokenException(goto_, "goto");
+		}
+
+		private String op1(String line) {
+			return line.split(" ")[2];
+		}
+
+		private String condition(String line) {
+			return line.split(" ")[3];
+		}
+
+		private String op2(String line) {
+			return line.split(" ")[4];
+		}
+
+		private String labelToJump(String line) {
+			return line.split(" ")[6];
+		}
 	},
 
 	/** Beginning of {@code if} block */
@@ -204,19 +302,19 @@ enum Statement {
 
 			final IfBlock block = new IfBlock();
 
-			final String[] tokens = line.split(" ");
-
-			String    op1, op2;
 			int       loc1, loc2;
 			Condition condition;
 
-			op1 = tokens[2];
-			loc1 = compiler.getSymbol(op1).location;
+			loc1 = compiler.getSymbol(op1(line)).location;
 
-			condition = Condition.of(tokens[3]);
+			try {
+				condition = Condition.of(condition(line));
+			} catch (InvalidConditionException e) {
+				// checkSyntax was called, this should never throw
+				throw new RuntimeException(e);
+			}
 
-			op2 = tokens[4];
-			loc2 = compiler.getSymbol(op2).location;
+			loc2 = compiler.getSymbol(op2(line)).location;
 
 			int location = 0;
 			switch (condition) {
@@ -259,6 +357,25 @@ enum Statement {
 			block.locationOfBranchToEndOfBlock = location;
 			compiler.pushBlock(block);
 		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+			SymbolType.assertNot(op1(line), SymbolType.LABEL);
+			SymbolType.assertNot(op2(line), SymbolType.LABEL);
+			Condition.of(condition(line));
+		}
+
+		private String op1(String line) {
+			return line.split(" ")[2];
+		}
+
+		private String condition(String line) {
+			return line.split(" ")[3];
+		}
+
+		private String op2(String line) {
+			return line.split(" ")[4];
+		}
 	},
 
 	/** Beginning of {@code else} block and end of {@code if} block */
@@ -276,6 +393,11 @@ enum Statement {
 
 			compiler.setBranchLocation(locationOfBranchToEnd, locationOfEnd + 1);
 		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+
+		}
 	},
 
 	/** End of {@code if} or {@code else} block */
@@ -289,6 +411,11 @@ enum Statement {
 			final int locationOfEnd = compiler.addInstruction(Instruction.NOOP.opcode());
 			compiler.setBranchLocation(locationOfBranchToEnd, locationOfEnd + 1);
 		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+
+		}
 	},
 
 	/** Start of {@code while} block */
@@ -298,19 +425,19 @@ enum Statement {
 
 			final WhileBlock block = new WhileBlock();
 
-			final String[] tokens = line.split(" ");
-
-			String    op1, op2;
 			int       loc1, loc2;
 			Condition condition;
 
-			op1 = tokens[2];
-			loc1 = compiler.getSymbol(op1).location;
+			loc1 = compiler.getSymbol(op1(line)).location;
 
-			condition = Condition.of(tokens[3]);
+			try {
+				condition = Condition.of(condition(line));
+			} catch (InvalidConditionException e) {
+				// checkSyntax was called, this should never throw
+				throw new RuntimeException(e);
+			}
 
-			op2 = tokens[4];
-			loc2 = compiler.getSymbol(op2).location;
+			loc2 = compiler.getSymbol(op2(line)).location;
 
 			int start    = 0;
 			int location = 0;
@@ -355,6 +482,25 @@ enum Statement {
 			block.locationOfBranchToEndOfBlock = location;
 			compiler.pushBlock(block);
 		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+			SymbolType.assertNot(op1(line), SymbolType.LABEL);
+			SymbolType.assertNot(op2(line), SymbolType.LABEL);
+			Condition.of(condition(line));
+		}
+
+		private String op1(String line) {
+			return line.split(" ")[2];
+		}
+
+		private String condition(String line) {
+			return line.split(" ")[3];
+		}
+
+		private String op2(String line) {
+			return line.split(" ")[4];
+		}
 	},
 
 	/** End of while block */
@@ -371,6 +517,11 @@ enum Statement {
 			final int locationOfBranchToEnd = block.locationOfBranchToEndOfBlock;
 			compiler.setBranchLocation(locationOfBranchToEnd, whileEndLocation + 1);
 		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+
+		}
 	},
 
 	/** End of program */
@@ -378,6 +529,11 @@ enum Statement {
 		@Override
 		public void evaluate(String line, SML_Compiler compiler) {
 			compiler.addInstruction(Instruction.HALT.opcode());
+		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+
 		}
 	},
 
@@ -387,6 +543,11 @@ enum Statement {
 		public void evaluate(String line, SML_Compiler compiler) {
 			compiler.addInstruction(Instruction.NOOP.opcode());
 		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+
+		}
 	},
 
 	/** Dump memory contents to screen */
@@ -394,6 +555,11 @@ enum Statement {
 		@Override
 		public void evaluate(String line, SML_Compiler compiler) {
 			compiler.addInstruction(Instruction.DUMP.opcode());
+		}
+
+		@Override
+		public void checkSyntax(String line) throws CompilerException {
+
 		}
 	};
 
@@ -424,13 +590,15 @@ enum Statement {
 	}
 
 	/**
-	 * Uses information from the SML_Compiler's Symbol Table to generate machine
-	 * code for a line of high-level code and stores it to the SML_Compiler.
+	 * Uses an {@code SML_Compiler} to generate machine code for a line of
+	 * high-level code for the {@code compiler}.
 	 *
-	 * @param line a line of high-level code
-	 * @param compiler TODO
+	 * @param line     a line of high-level code
+	 * @param compiler the compiler for which to generate machine code
 	 */
-	protected abstract void evaluate(String line, SML_Compiler compiler);
+	public abstract void evaluate(String line, SML_Compiler compiler);
+
+	public abstract void checkSyntax(String line) throws CompilerException;
 
 	/**
 	 * Returns the {@code Statement} that has the given {@code identifier}.
@@ -438,9 +606,17 @@ enum Statement {
 	 * @param identifier the {@code identifier} of the Statement
 	 *
 	 * @return the Statement with that identifier
+	 *
+	 * @throws InvalidStatementException when there is no Statement with the given
+	 *                                   identifier
 	 */
-	public static Statement of(String identifier) {
-		return Statement.map.get(identifier);
+	public static Statement of(String identifier) throws InvalidStatementException {
+
+		final Statement statement = Statement.map.get(identifier);
+		if (statement == null)
+			throw new InvalidStatementException(identifier);
+
+		return statement;
 	}
 
 	Statement(String identifier, int length, boolean isConstructor) {
