8b1ede34b743652c3e9f8043c7ab43bc4f87abe0
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-02 16:38:03 +0300

    doc

--- src/compiler/symboltable/package-info.java
+++ src/compiler/symboltable/package-info.java
@@ -2,6 +2,8 @@
  * Defines a {@link compiler.symboltable.SymbolTable SymbolTable} which is used
  * during compilation to keep track of {@link compiler.symboltable.SymbolInfo
  * information} about different Symbols found in a high-level-language program.
+ * These Symbols have different {@link compiler.symboltable.SymbolType Types}
+ * and are defined by a regex that every Symbol of this Type matches.
  *
  * @author Alex Mandelias
  */
