8e45e4e72f703a8de309ab4208b85b277d779c39
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-02 16:06:15 +0300

    doc + removed quotes

--- src/compiler/exceptions/UnexpectedTokenException.java
+++ src/compiler/exceptions/UnexpectedTokenException.java
@@ -8,13 +8,14 @@ package compiler.exceptions;
 public class UnexpectedTokenException extends CompilerException {
 
 	/**
-	 * Constructs the exception with information about the {@code found} and the
-	 * {@code expected} Tokens.
+	 * Constructs the exception with the {@code found} and the {@code expected}
+	 * Tokens. The Strings for the Tokens will be formatted as-is in the exception
+	 * message, no quotes will be added around them.
 	 *
 	 * @param found    the token that was found
 	 * @param expected the token that was expected
 	 */
 	public UnexpectedTokenException(String found, String expected) {
-		super("Unexpected token '%s'. Expeted: '%s'", found, expected);
+		super("Unexpected token %s. Expeted: %s", found, expected);
 	}
 }
