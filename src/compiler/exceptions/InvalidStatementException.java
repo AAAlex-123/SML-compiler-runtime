bf768e234ea92e6080b5dfd63e64e7317740089b
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-02 15:57:48 +0300

    doc

--- src/compiler/exceptions/InvalidStatementException.java
+++ src/compiler/exceptions/InvalidStatementException.java
@@ -1,15 +1,15 @@
 package compiler.exceptions;
 
 /**
- * Thrown when an high-level-program statement was read but its identifier
- * doesn't match any {@link runtime.Statement Statement}.
+ * Thrown when a high-level-program statement was read but its identifier
+ * doesn't match any of the Compiler's {@code Statements}.
  *
  * @author Alex Mandelias
  */
 public class InvalidStatementException extends CompilerException {
 
 	/**
-	 * Constructs the Exception with information about the {@code identifier}.
+	 * Constructs the Exception with the {@code identifier}.
 	 *
 	 * @param identifier the identifier for which there is no {@code Statement}
 	 */
