66ab7c788e1622390b28546236106d968137dca7
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-02 10:16:47 +0300

    doc + cleanup
     - added throws statements in the static method declarations
     - made the Pattern public for external use

--- src/compiler/symboltable/SymbolType.java
+++ src/compiler/symboltable/SymbolType.java
@@ -11,7 +11,10 @@ import compiler.exceptions.UnexpectedTokenException;
 /**
  * The Type of a {@link SymbolInfo Symbol} found in a high-level-language
  * program. {@code Symbols} may represent Variables, Constants or Labels and
- * have the corresponding type.
+ * have the corresponding type. The Type is determined using a regular
+ * expression that a {@code Symbol} must match. The {@code assert} methods use
+ * them and may throw an {@link compiler.exceptions.CompilerException
+ * CompmilerException} associated with each type when the assertion fails.
  *
  * @author Alex Mandelias
  */
@@ -26,37 +29,56 @@ public enum SymbolType {
 	/** Symbol for a Line */
 	LABEL('L', ":\\w*", NotALabelException.class);
 
-	private char c;
-	private Pattern pattern;
-	private Class<? extends CompilerException> exc;
+	/** The Pattern that Symbols of this Type match */
+	public final Pattern pattern;
 
-	private SymbolType(char c, String regex, Class<? extends CompilerException> exc) {
+	private final char                               c;
+	private final Class<? extends CompilerException> exc;
+
+	SymbolType(char c, String regex, Class<? extends CompilerException> exc) {
 		this.c = c;
 		pattern = Pattern.compile(regex);
 		this.exc = exc;
 	}
 
-	public static void assertType(String symbol, SymbolType type) throws NotAVariableException {
-		if (!type.pattern.matcher(symbol).find())
-			try {
-				throw type.exc.getDeclaredConstructor().newInstance();
-			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-					| InvocationTargetException | NoSuchMethodException | SecurityException | CompilerException e) {
-				throw new RuntimeException(e);
-			}
-	}
-
-	public static void assertNot(String symbol, SymbolType type) throws NotAVariableException {
-		if (type.pattern.matcher(symbol).find())
-			try {
-				throw new UnexpectedTokenException(symbol, "not a " + type);
-			} catch (IllegalArgumentException | SecurityException | CompilerException e) {
-				throw new RuntimeException(e);
-			}
-	}
-
 	@Override
 	public String toString() {
 		return String.format("%c", c);
+	}
+
+	/**
+	 * Asserts that the {@code symbol} matches a specific {@code type}.
+	 *
+	 * @param symbol the symbol
+	 * @param type   the expected type of the symbol
+	 *
+	 * @throws CompilerException if the symbol doesn't match the type
+	 */
+	public static void assertType(String symbol, SymbolType type) throws CompilerException {
+		if (!type.pattern.matcher(symbol).find())
+			try {
+				throw type.exc.getDeclaredConstructor(String.class).newInstance();
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+			        | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+				throw new RuntimeException(e);
+			}
+	}
+
+	/**
+	 * Asserts that the {@code symbol} does not match a specific {@code type}.
+	 *
+	 * @param symbol the symbol
+	 * @param type   the expected type of the symbol
+	 *
+	 * @throws UnexpectedTokenException if the symbol matches the type
+	 */
+	public static void assertTypeNot(String symbol, SymbolType type)
+	        throws UnexpectedTokenException {
+		if (type.pattern.matcher(symbol).find())
+			try {
+				throw new UnexpectedTokenException(symbol, "not a " + type);
+			} catch (IllegalArgumentException | SecurityException e) {
+				throw new RuntimeException(e);
+			}
 	}
 }
