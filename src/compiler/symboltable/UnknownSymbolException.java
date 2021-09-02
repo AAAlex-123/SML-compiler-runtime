d9eef6ac294f7c38432f5cae43961a645873c508
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-02 16:36:41 +0300

    doc

--- src/compiler/symboltable/UnknownSymbolException.java
+++ src/compiler/symboltable/UnknownSymbolException.java
@@ -1,31 +1,37 @@
 package compiler.symboltable;
 
+import java.util.Arrays;
+
 /**
- * Thrown when a high-level-language {@code symbol} is missing from a Symbol
- * Table.
+ * Thrown when a high-level-language {@code symbol} is missing from a
+ * {@code Symbol Table}.
  *
  * @author Alex Mandelias
  */
 public class UnknownSymbolException extends RuntimeException {
 
 	/**
-	 * Constructs the Exception with the given {@code symbol}. The symbol can be of
-	 * any type.
+	 * Constructs the Exception with a {@code symbol} of any Type.
 	 *
 	 * @param symbol the missing symbol
+	 *
+	 * @see SymbolType
 	 */
 	public UnknownSymbolException(String symbol) {
-		super(String.format("Can't find Symbol: %s", symbol));
+		super(String.format("Can't find Symbol '%s'", symbol));
 	}
 
 	/**
-	 * Constructs the Exception with the given {@code symbol}. The symbol can be
-	 * only one of the types provided.
+	 * Constructs the Exception with a {@code symbol}. The symbol can be only one of
+	 * the provided Types.
 	 *
 	 * @param symbol the missing symbol
 	 * @param types  the possible types of the symbol
+	 *
+	 * @see SymbolType
 	 */
 	public UnknownSymbolException(String symbol, SymbolType... types) {
-		super(String.format("Can't find Symbol: %s of Type(s): %s", symbol, types));
+		super(String.format("Can't find Symbol '%s' of Type(s): %s", symbol,
+		        Arrays.toString(types)));
 	}
 }
