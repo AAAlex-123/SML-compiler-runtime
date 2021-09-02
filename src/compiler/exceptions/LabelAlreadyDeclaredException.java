db21881d539931ae2da4db2aec2e2bbf762d99ee
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-02 16:11:56 +0300

    a lot of doc

--- src/compiler/exceptions/LabelAlreadyDeclaredException.java
+++ src/compiler/exceptions/LabelAlreadyDeclaredException.java
@@ -1,18 +1,18 @@
 package compiler.exceptions;
 
 /**
- * Thrown when an already-declared label is re-declared.
+ * Thrown when an already-declared Label is re-declared.
  *
  * @author Alex Mandelias
  */
 public class LabelAlreadyDeclaredException extends CompilerException {
 
 	/**
-	 * Constructs the exception with the {@code label} that was already declared.
+	 * Constructs the exception with a {@code label}.
 	 *
-	 * @param symbol the label that was already declared
+	 * @param label the label that was already declared
 	 */
-	public LabelAlreadyDeclaredException(String symbol) {
-		super("label '%s' already declared", symbol);
+	public LabelAlreadyDeclaredException(String label) {
+		super("label '%s' already declared", label);
 	}
 }
