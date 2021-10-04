b10c09f2a42ae22a479131e915159affd8ef625d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-10-04 16:55:39 +0300

    Doc + file structure (#42)
     - Moved example files to the example_files directory
     - Rename out.sml to out.txt to avoid name collisions
     - Adds missing package-info files and fixes doc
     - Added generated .html javadoc files

---
+++ src/compiler/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Defines the classes necessary to compile high-level-language code.
+ *
+ * @author Alex Mandelias
+ *
+ * @see compiler.SML_Compiler
+ */
+package compiler;
---
+++ src/runtime/exceptions/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Defines the checked Exceptions that may occur during execution. All of them
+ * are a concrete subclass of the {@link runtime.exceptions.ExecutionException
+ * ExecutionException}. Exceptions are used (instead of simple error messages)
+ * to allow for uniform handling of errors during parsing and clearer control
+ * flow.
+ *
+ * @author Alex Mandelias
+ */
+package runtime.exceptions;
